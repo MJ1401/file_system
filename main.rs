a7e6a16516a8429e502204e2487c04f77f05a733
MJ1401 <78559833+MJ1401@users.noreply.github.com>
2023-03-30 16:21:10 -0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,162 @@
+#![cfg_attr(not(test), no_std)]
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub enum FileSystemResult<T: Copy + Clone> {
+    Ok(T),
+    Err(FileSystemError),
+}
+
+impl<T: Copy + Clone> FileSystemResult<T> {
+    pub fn unwrap(&self) -> T {
+        match self {
+            FileSystemResult::Ok(v) => *v,
+            FileSystemResult::Err(e) => panic!("Error: {e:?}"),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub enum FileSystemError {
+    FileNotFound,
+    FileNotOpen,
+    NotOpenForRead,
+    NotOpenForWrite,
+    TooManyOpen,
+    TooManyFiles,
+    AlreadyOpen,
+    DiskFull,
+    FileTooBig,
+    FilenameTooLong,
+    InvalidFileDescriptor(usize),
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct FileInfo<const MAX_BLOCKS: usize, const BLOCK_SIZE: usize> {
+    inode: Inode<MAX_BLOCKS, BLOCK_SIZE>,
+    inode_num: usize,
+    current_block: usize,
+    offset: usize,
+    writing: bool,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct Inode<const MAX_BLOCKS: usize, const BLOCK_SIZE: usize> {
+    bytes_stored: u16,
+    blocks: [u8; MAX_BLOCKS],
+}
+
+const INODE_FULL_BLOCK: usize = 0;
+const DATA_FULL_BLOCK: usize = INODE_FULL_BLOCK + 1;
+const INODE_TABLE_START: usize = DATA_FULL_BLOCK + 1;
+
+#[derive(core::fmt::Debug)]
+pub struct FileSystem<
+    const MAX_OPEN: usize,
+    const BLOCK_SIZE: usize,
+    const NUM_BLOCKS: usize,
+    const MAX_FILE_BLOCKS: usize,
+    const MAX_FILE_BYTES: usize,
+    const MAX_FILES_STORED: usize,
+    const MAX_FILENAME_BYTES: usize,
+> {
+    open: [Option<FileInfo<MAX_FILE_BLOCKS, BLOCK_SIZE>>; MAX_OPEN],
+    disk: ramdisk::RamDisk<BLOCK_SIZE, NUM_BLOCKS>,
+    block_buffer: [u8; BLOCK_SIZE],
+    file_content_buffer: [u8; MAX_FILE_BYTES],
+}
+
+impl<
+        const MAX_OPEN: usize,
+        const BLOCK_SIZE: usize,
+        const NUM_BLOCKS: usize,
+        const MAX_FILE_BLOCKS: usize,
+        const MAX_FILE_BYTES: usize,
+        const MAX_FILES_STORED: usize,
+        const MAX_FILENAME_BYTES: usize,
+    >
+    FileSystem<
+        MAX_OPEN,
+        BLOCK_SIZE,
+        NUM_BLOCKS,
+        MAX_FILE_BLOCKS,
+        MAX_FILE_BYTES,
+        MAX_FILES_STORED,
+        MAX_FILENAME_BYTES,
+    >
+{
+    pub fn new(disk: ramdisk::RamDisk<BLOCK_SIZE, NUM_BLOCKS>) -> Self {
+        assert_eq!(MAX_FILE_BYTES, MAX_FILE_BLOCKS * BLOCK_SIZE);
+        assert!(NUM_BLOCKS <= u8::MAX as usize);
+        assert!(MAX_FILE_BYTES <= u16::MAX as usize);
+        let block_bits = BLOCK_SIZE * 8;
+        assert!(MAX_FILES_STORED <= block_bits);
+        assert!(MAX_FILES_STORED <= u16::MAX as usize);
+        let result = Self {
+            open: [None; MAX_OPEN],
+            disk,
+            block_buffer: [0; BLOCK_SIZE],
+            file_content_buffer: [0; MAX_FILE_BYTES],
+        };
+        assert!(result.num_inode_blocks() * 2 < NUM_BLOCKS);
+        assert!(result.num_data_blocks() <= block_bits);
+        assert_eq!(
+            result.num_data_blocks() + result.num_inode_blocks() + 2,
+            NUM_BLOCKS
+        );
+        assert!(result.num_inode_entries() <= u16::MAX as usize);
+        assert!(result.num_inode_blocks() <= MAX_FILE_BLOCKS);
+        result
+    }
+
+    pub fn max_file_size(&self) -> usize {
+        MAX_FILE_BLOCKS * BLOCK_SIZE
+    }
+
+    pub fn num_inode_bytes(&self) -> usize {
+        2 + MAX_FILE_BLOCKS
+    }
+
+    pub fn inodes_per_block(&self) -> usize {
+        BLOCK_SIZE / self.num_inode_bytes()
+    }
+
+    pub fn num_inode_blocks(&self) -> usize {
+        MAX_FILES_STORED / self.inodes_per_block()
+    }
+
+    pub fn num_data_blocks(&self) -> usize {
+        NUM_BLOCKS - self.num_inode_blocks() - 2
+    }
+
+    pub fn num_inode_entries(&self) -> usize {
+        self.inodes_per_block() * self.num_inode_blocks() * self.num_inode_bytes()
+    }
+
+    pub fn first_data_block(&self) -> usize {
+        2 + self.num_inode_blocks()
+    }
+
+    pub fn open_read(&mut self, filename: &str) -> FileSystemResult<usize> {
+        todo!("Your code here");
+    }
+
+    pub fn open_create(&mut self, filename: &str) -> FileSystemResult<usize> {
+        todo!("Your code here");
+    }
+
+    pub fn open_append(&mut self, filename: &str) -> FileSystemResult<usize> {
+        todo!("Your code here");
+    }
+
+    pub fn close(&mut self, fd: usize) -> FileSystemResult<()> {
+        todo!("Your code here");
+    }
+
+    pub fn read(&mut self, fd: usize, buffer: &mut [u8]) -> FileSystemResult<usize> {
+        todo!("Your code here");
+    }
+
+    pub fn write(&mut self, fd: usize, buffer: &[u8]) -> FileSystemResult<()> {
+        todo!("Your code here");
+    }
+}
---
+++ tests.rs
@@ -0,0 +1,29 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn make_small_fs() -> FileSystem<16, 64, 255, 8, 512, 32, 8> {
+        FileSystem::new(ramdisk::RamDisk::new())
+    }
+
+    #[test]
+    fn test_empty() {
+        let mut sys = make_small_fs();
+        assert!(!sys.directory_exists());
+        assert!(sys.inode_for("test") == FileSystemResult::Err(FileSystemError::FileNotFound));
+    }
+
+    #[test]
+    fn test_short_write() {
+        let mut sys = make_small_fs();
+        let f1 = sys.open_create("one.txt").unwrap();
+        sys.write(f1, "This is a test.".as_bytes()).unwrap();
+        let mut buffer = [0; 50];
+        sys.close(f1).unwrap();
+        let f2 = sys.open_read("one.txt").unwrap();
+        let bytes_read = sys.read(f2, &mut buffer).unwrap();
+        assert_eq!(bytes_read, 15);
+        let s = core::str::from_utf8(&buffer[0..bytes_read]).unwrap();
+        assert_eq!(s, "This is a test.");
+    }
+}
